7aefc0f1871e76acaa68a4af5ca7e4aeeba8f6e1
kridesav <savolainenkristjan@gmail.com>
2024-03-13 13:31:51 +0200

    assigment 14

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import * as Speech from 'expo-speech';
+import * as React from 'react';
 
 export default function App() {
+  const [input, setInput] = React.useState('');
+
+  const speak = () => {
+    Speech.speak(input);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput onChangeText={(text) => setInput(text)} value={input} placeholder="Type here" style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, margin: 10, padding: 10, textAlign: 'center' }} />
+      <Button title="Press to hear text" onPress={speak} />
       <StatusBar style="auto" />
     </View>
   );
